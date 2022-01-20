583e2e4e8e36bf5a898bd204f0459c559b989322
Nikita Patel <gniki0830@gmail.com>
2022-01-20 15:46:55 +0530

    home(html-ejs)

--- ejs(homepage-ejs)/src/configs/db.js
+++ ejs(homepage-ejs)/src/configs/db.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = connect = () => {
-  return mongoose.connect(
-    "mongodb+srv://mamaearth:<mamaearth123>@mamaearth-fullstack.qvqsp.mongodb.net/test"
-  );
+  return mongoose.connect("mongodb://127.0.0.1:27017/MAMAE_BACK");
 };
